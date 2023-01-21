a153e11e3812b8a83fe82fc581f2d162f8db43ea
Sultan Soltanov <soltanoff@tuta.io>
2023-01-21 19:55:46 +0500

    Add get and post mapping

--- src/main/java/org/corpspace/teamspace/web/rest/UserResource.java
+++ src/main/java/org/corpspace/teamspace/web/rest/UserResource.java
@@ -10,9 +10,10 @@ package org.corpspace.teamspace.web.rest;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 import javax.validation.Valid;
+import org.corpspace.teamspace.domain.User;
 import org.corpspace.teamspace.repository.UserRepository;
-import org.corpspace.teamspace.service.dto.RoleDTO;
 import org.corpspace.teamspace.service.dto.UserDTO;
 import org.corpspace.teamspace.service.impl.UserServiceImpl;
 import org.corpspace.teamspace.web.rest.errors.BadRequestAlertException;
@@ -20,10 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.jhipster.web.util.HeaderUtil;
 
 @RestController
@@ -53,15 +51,28 @@ public class UserResource {
      * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @PostMapping("/users")
-    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO) throws URISyntaxException {
+    public ResponseEntity<User> createUser(@Valid @RequestBody UserDTO userDTO) throws URISyntaxException {
         log.debug("REST request to save User : {}", userDTO);
+
         if (userDTO.getId() != null) {
-            throw new BadRequestAlertException("A new role cannot already have an ID", ENTITY_NAME, "id exists");
+            throw new BadRequestAlertException("A new role cannot already have an ID", ENTITY_NAME, "idexists");
         }
-        UserDTO result = userService.save(userDTO);
+
+        User result = userService.createUser(userDTO);
         return ResponseEntity
             .created(new URI("/api/v1/users/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
             .body(result);
+    }
+
+    /**
+     * GET /users : get all the users.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body all the users,
+     * or with status {@code 404 (Not Found)} if there are no users
+     */
+    @GetMapping("/users")
+    public List<UserDTO> getAllUser() {
+        log.debug("REST request to get all User");
+        return userService.findAll();
     }
 }
