b95fcd5681f6d1aeb68c747011e2214c96955042
Sultan Soltanov <soltanoff@tuta.io>
2023-01-22 12:09:35 +0500

    Security fixing

--- src/main/java/org/corpspace/teamspace/config/SecurityConfiguration.java
+++ src/main/java/org/corpspace/teamspace/config/SecurityConfiguration.java
@@ -20,9 +20,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.web.filter.CorsFilter;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
-import tech.jhipster.config.JHipsterProperties;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
@@ -30,6 +31,7 @@ import tech.jhipster.config.JHipsterProperties;
 public class SecurityConfiguration {
 
     private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
     private final SecurityProblemSupport problemSupport;
 
     @Bean
@@ -37,9 +39,10 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-    public SecurityConfiguration(TokenProvider tokenProvider, SecurityProblemSupport problemSupport) {
+    public SecurityConfiguration(TokenProvider tokenProvider, SecurityProblemSupport problemSupport, CorsFilter corsFilter) {
         this.tokenProvider = tokenProvider;
         this.problemSupport = problemSupport;
+        this.corsFilter = corsFilter;
     }
 
     @Bean
@@ -48,22 +51,33 @@ public class SecurityConfiguration {
         http
             .csrf()
             .disable()
+            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
             .exceptionHandling()
                 .authenticationEntryPoint(problemSupport)
                 .accessDeniedHandler(problemSupport)
         .and()
+            .headers()
+            .contentSecurityPolicy("default-src 'self'; frame-src 'self' https://*.corpspace.org; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.corpspace.org; style-src 'self' 'unsafe-inline 'https://*.corpspace.org'; img-src 'self' data: https://*.corpspace.org; font-src 'self' https://*.corpspace.org")
+        .and()
+            .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
+        .and()
+            .featurePolicy("geolocation 'none'; midi 'none'; sync-xhr 'none'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; speaker 'none'; fullscreen 'self'; payment 'none'")
+        .and()
+            .frameOptions()
+            .deny()
+        .and()
             .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
             .authorizeRequests()
             .antMatchers("/api/authenticate").permitAll()
+            .antMatchers("/api/register").permitAll()
+            .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/api/**").authenticated()
-            .antMatchers("/management/health").permitAll()
-            .antMatchers("/management/health/**").permitAll()
-            .antMatchers("/management/info").permitAll()
-            .antMatchers("/management/prometheus").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+        .and()
+            .httpBasic()
         .and()
             .apply(securityConfigurerAdapter());
         return http.build();
